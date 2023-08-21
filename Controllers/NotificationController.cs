1f07e2848f88688d53f5c4fb19f3bd0a6c52db65
Sugumar Prabhakar <sugumar_prabhakar@trimble.com>
2023-08-21 15:38:58 +1200

    First commit

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,81 @@
+﻿using Flurl.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace google_chat_dotnet_webhook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        [HttpPost("send")]
+        public async Task SendNotification(NotificationDTO notificationDTO)
+        {
+            var cardsV2 = new List<CardsV2>
+            {
+                new CardsV2
+                {
+                    cardId = $"MessageFrom{notificationDTO.SentBy}",
+                    card = new Card
+                    {
+                        header = new Header
+                        {
+                            title = notificationDTO.SentByTitle,
+                            subtitle = $"sent at {DateTime.Now}",
+                        },
+                        sections = new List<Section>
+                        {
+                            new Section
+                            {
+                                widgets = new List<Widget>
+                                {
+                                    new Widget
+                                    {
+                                        decoratedText =new DecoratedText
+                                        {
+                                            text = notificationDTO.SentByTitle,
+                                            startIcon = new StartIcon
+                                            {
+                                                knownIcon = "BOOKMARK"
+                                            }
+                                        }
+                                    },
+                                    new Widget
+                                    {
+                                        decoratedText =new DecoratedText
+                                        {
+                                            text = notificationDTO.Message,
+                                            startIcon = new StartIcon
+                                            {
+                                                knownIcon = "EMAIL"
+                                            }
+                                        }
+                                    },
+                                }
+                            },
+                            new Section
+                            {
+                                widgets = new List<Widget>
+                                {
+                                    new Widget
+                                    {
+                                        decoratedText =new DecoratedText
+                                        {
+                                            topLabel = "notification sent by",
+                                            text = notificationDTO.SentBy.ToUpper(),
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var newMessage = new GoogleChatCardMessage() { cardsV2 = cardsV2 };
+
+            await notificationDTO.WebHookURL.PostJsonAsync(newMessage);
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Models.cs
@@ -0,0 +1,98 @@
+﻿namespace google_chat_dotnet_webhook;
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+public class Action
+{
+    public string function { get; set; }
+    public List<Parameter> parameters { get; set; }
+}
+
+public class Button
+{
+    public string text { get; set; }
+    public OnClick onClick { get; set; }
+}
+
+public class ButtonList
+{
+    public List<Button> buttons { get; set; }
+}
+
+public class Card
+{
+    public Header header { get; set; }
+    public List<Section> sections { get; set; }
+}
+
+public class CardsV2
+{
+    public string cardId { get; set; }
+    public Card card { get; set; }
+}
+
+public class DecoratedText
+{
+    public StartIcon startIcon { get; set; }
+    public string text { get; set; }
+    public string topLabel { get; set; }
+}
+
+public class Header
+{
+    public string title { get; set; }
+    public string subtitle { get; set; }
+    public string imageUrl { get; set; }
+    public string imageType { get; set; }
+    public string imageAltText { get; set; }
+}
+
+public class OnClick
+{
+    public OpenLink openLink { get; set; }
+    public Action action { get; set; }
+}
+
+public class OpenLink
+{
+    public string url { get; set; }
+}
+
+public class Parameter
+{
+    public string key { get; set; }
+    public string value { get; set; }
+}
+
+public class GoogleChatCardMessage
+{
+    public List<CardsV2> cardsV2 { get; set; }
+}
+
+public class Section
+{
+    public string header { get; set; }
+    public bool collapsible { get; set; }
+    public int uncollapsibleWidgetsCount { get; set; }
+    public List<Widget> widgets { get; set; }
+}
+
+public class StartIcon
+{
+    public string knownIcon { get; set; }
+}
+
+public class Widget
+{
+    public DecoratedText decoratedText { get; set; }
+    public ButtonList buttonList { get; set; }
+}
+
+
+public class NotificationDTO
+{
+    public string WebHookURL { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public string SentBy { get; set; } = string.Empty;
+    public string SentByTitle { get; set; } = string.Empty;
+}
